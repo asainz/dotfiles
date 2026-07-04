e26efd02d6fd2c85695904d659bcfe992e6b5876
Andrés Sainz de Aja <andressainz@gmail.com>
2026-07-04 15:10:47 +0200

    chore: add zen browser cask

--- scripts/cask.sh
+++ scripts/cask.sh
@@ -26,6 +26,7 @@ brew install --cask imageoptim
 brew install --cask sublime-merge
 brew install --cask google-chrome
 brew install --cask google-chrome@canary
+brew install --cask zen
 brew install --cask docker-desktop
 brew install --cask google-cloud-sdk
 brew install --cask yaak # postman replacement
